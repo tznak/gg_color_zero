6e5e8b91be27e51828424788f94cd43867680d64
Tznak <tznakz@gmail.com>
2019-10-05 00:10:36 +0200

    init commiti

---
+++ Kernel32.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Diagnostics.CodeAnalysis;
+using System.Runtime.InteropServices;
+
+namespace gg_color_zero
+{
+    [SuppressMessage("ReSharper", "InconsistentNaming")]
+    public static class Kernel32
+    {
+        [Flags]
+        public enum AccessRights
+        {
+            PROCESS_VM_OPERATION = 0x0008,
+            PROCESS_VM_READ      = 0x0010,
+            PROCESS_VM_WRITE     = 0x0020
+        }
+        
+        [DllImport("kernel32.dll")] public static extern IntPtr OpenProcess(AccessRights dwDesiredAccess, bool bInheritHandle, int dwProcessId);
+        [DllImport("kernel32.dll")] public static extern bool ReadProcessMemory(IntPtr hProcess, IntPtr lpBaseAddress, byte[] lpBuffer, int nSize, ref int lpNumberOfBytesRead);
+        [DllImport("kernel32.dll")] public static extern bool WriteProcessMemory(IntPtr hProcess, IntPtr lpBaseAddress, byte[] lpBuffer, int nSize, ref int lpNumberOfBytesWritten);
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Diagnostics;
+using System.Linq;
+using static gg_color_zero.Kernel32;
+using static gg_color_zero.Kernel32.AccessRights;
+
+namespace gg_color_zero
+{
+    internal static class Program
+    {
+        public static void Main(string[] args)
+        {
+            const int online_skins = 0x1AC87B5;
+            const AccessRights access_flags = PROCESS_VM_WRITE | PROCESS_VM_OPERATION;
+
+            var processes = Process.GetProcessesByName("GuiltyGearXrd");
+            if (processes.Length != 0 && processes[0]?.MainModule != null)
+            {
+                var process_handle = OpenProcess(access_flags, false, processes[0].Id);
+                var base_address = processes[0].MainModule.BaseAddress;
+                var skins = Enumerable.Repeat<byte>(0xFF, 25).ToArray();
+
+                var bytes_written = 0;
+                WriteProcessMemory
+                (
+                    process_handle,
+                    base_address + online_skins,
+                    skins, skins.Length,
+                    ref bytes_written
+                );
+
+                Console.WriteLine("All online colors set to duplicate.");
+            }
+            else
+            {
+                Console.WriteLine("Could not find the Guilty Gear process.");
+            }
+
+            Console.WriteLine("Press any key to continue.");
+            Console.ReadKey();
+        }
+    }
+}
